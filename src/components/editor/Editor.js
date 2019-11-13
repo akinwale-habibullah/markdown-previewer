076a0eb26b260f99e286fcfb678b9597f1c01992
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 16:33:28 +0100

    handle text change in markdown editor in editor component

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
 
 const EditorComponent = (props) => {
+  function handleTextChange(e){
+    const markdown = e.target.value;
+    props.submitMarkDown(markdown);
+  }
+
   return (
     <div>
-      <h4>Enter markdown in this text area</h4>
-      <textarea placeholder="enter markdown here"></textarea>
+      <h4>Markdown Editor</h4>
+      <div className="input-field col s12">
+        <textarea id="textarea1" className="materialize-textarea" onKeyUp={handleTextChange}></textarea>
+        <label htmlFor="textarea1">Enter markdown here</label>
+      </div>
     </div>
   )
 }
