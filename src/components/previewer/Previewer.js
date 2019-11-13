447aec49ab9ceaee793190ea0343bdfc067b8d2a
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 16:36:57 +0100

    display html output in previewer component

--- src/components/previewer/Previewer.js
+++ src/components/previewer/Previewer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Previewer.css';
 
 const PreviewerComponent = (props) => {
+  const html = {
+    __html: props.htmlOutput
+  }
+  console.log(html);
   return (
     <div>
       <h4>Previewer</h4>
-      <div></div>
+      <div dangerouslySetInnerHTML={html} id="preview-div"></div>
+      {/* {props.htmlOutput ? <div dangerouslySetInnerHTML={html}/> : <p>'No item yet'</p>} */}
     </div>
   )
 }
