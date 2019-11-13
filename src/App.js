2bb7fd6c56766d368993c7b65c07604e3b4c36e4
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 16:32:57 +0100

    change App component to class and add state with state update functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { Remarkable } from 'remarkable';
 import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
 import EditorComponent from './components/editor/Editor';
 import PreviewerComponent from './components/previewer/Previewer';
 
-function App() {
-  return (
-    <div className="container" style={{textAlign: "center"}}>
-      <h1>Markdown previewer</h1>
-      <div className="row">
-        <div className="col s6">
-          <EditorComponent />
-        </div>
-        <div className="col s6">
-          <PreviewerComponent />
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      htmlOutput: ''
+    }
+
+    this.submitMarkDown = this.submitMarkDown.bind(this);
+  }
+
+  submitMarkDown(str){
+    const md = new Remarkable({
+      html:  true,
+      breaks: true,
+    });
+    const htmlOutput = md.render(str);
+    this.setState((state, props) => ({
+      htmlOutput
+    }))
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1 id="app-header">Markdown previewer</h1>
+        <div className="row">
+          <div className="col s12 m6 l5">
+            <EditorComponent submitMarkDown={this.submitMarkDown}/>
+          </div>
+          <div className="col s12 m6 l7">
+            <PreviewerComponent htmlOutput={this.state.htmlOutput}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
